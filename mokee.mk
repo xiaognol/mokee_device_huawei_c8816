8290958d27485e22edb098a5f026b85cd7a63008
xiaognol <317065820@qq.com>
2014-09-27 01:01:13 +0800

    change to mokee device

--- mokee.mk
+++ mokee.mk
@@ -15,20 +15,20 @@
 #
 
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/cdma.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := c8816
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common mk stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/c8816/device_c8816.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := c8816
-PRODUCT_NAME := cm_c8816
-PRODUCT_BRAND := Huawei
+PRODUCT_NAME := mk_c8816
+PRODUCT_BRAND := huawei
 PRODUCT_MODEL := HUAWEI C8816
 PRODUCT_MANUFACTURER := HUAWEI
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo mk_c8816-userdebug
